ad1fd3db3c5b47c0becbf34d9833a5f148708d27
alessandroluissantos <118467387+alessandroluissantos@users.noreply.github.com>
2025-07-22 10:13:53 -0300

    atual1

---
+++ script.js
@@ -0,0 +1,10 @@
+const temperaturas = [
+    {id: 1, temperatura:23.9}
+    {id: 2, temperatura:25.4}
+    {id: 3, temperatura:21.1}
+    {id: 4, temperatura:32.7}
+]
+
+function simularLeitura() {
+    alert("Chegou na função")
+}
